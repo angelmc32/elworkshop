adc3a4ede94236c2043e855cf36d7414847473d8
Mel <angel.melendez.c@gmail.com>
2020-08-28 09:52:25 -0500

    :rocket: Build for heroku deployment

---
+++ elworkshop-backend/public/precache-manifest.1dda2e718a5860e5675fcba0a75367a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f10208391318b31380bd573372e7e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c093492d8e38ad1481f",
+    "url": "/static/css/2.485194a1.chunk.css"
+  },
+  {
+    "revision": "93fe738c9b15d29066ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9c093492d8e38ad1481f",
+    "url": "/static/js/2.78f60dff.chunk.js"
+  },
+  {
+    "revision": "c2a34665f82d5959174cda9c6921338f",
+    "url": "/static/js/2.78f60dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93fe738c9b15d29066ef",
+    "url": "/static/js/main.d407621c.chunk.js"
+  },
+  {
+    "revision": "a321528cc4a2bb388807",
+    "url": "/static/js/runtime-main.8730af60.js"
+  }
+]);
